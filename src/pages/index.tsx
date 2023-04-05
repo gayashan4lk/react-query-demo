80d2f0cfa1c910c4a1d6e78e95c539aa3a2251db
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-04-05 15:31:06 +0530

    create the most basic react query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,37 @@
+"use client"
 import { Welcome } from "../components/Welcome"
+import { useQuery, useMutation } from "@tanstack/react-query"
+
+const posts = [
+  { id: 1, title: "Hello Next.js" },
+  { id: 2, title: "Next.js is awesome" },
+]
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration))
+}
 
 export default function Home() {
-  return <Welcome />
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...posts]),
+  })
+
+  // const postsQuery = useQuery({
+  //   queryKey: ["posts"],
+  //   queryFn: () => Promise.reject("This is a bad error"),
+  // })
+
+  if (postsQuery.isLoading) return <div>Loading...</div>
+
+  if (postsQuery.isError)
+    return <pre>Oh no, Error occured! {JSON.stringify(postsQuery.error)}</pre>
+
+  return (
+    <div>
+      {postsQuery.data.map((post: any) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  )
 }
